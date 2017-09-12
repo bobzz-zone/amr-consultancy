c1230b7e617bfff9f93085c79a1836689fb72903
bobzz-zone <bobzz.zone@gmail.com>
2017-09-13 01:36:26 +0700

    gg

--- amr_consultancy/amr_consultancy/doctype/consultant_deployment_report/consultant_deployment_report.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2017, bobzz.zone@gmail.com and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Consultant Deployment report', {
-	refresh: function(frm) {
-
-	}
-});
